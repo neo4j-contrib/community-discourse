d90e8b8d24986037e9d9aaa277d79b0021143c64
Mark Needham <mark.needham@neotechnology.com>
2018-08-21 18:30:02 +0000

    latest updates

--- handler.py
+++ handler.py
@@ -1,11 +1,89 @@
 import json
 import os
-from base64 import b64decode
-from base64 import b64encode
 
-import boto3
 import requests
+from neo4j.v1 import GraphDatabase
 from requests_toolbelt import MultipartEncoder
+
+from util.encryption import decrypt_value_str
+
+host_port = decrypt_value_str(os.environ['GRAPHACADEMY_DB_HOST_PORT'])
+user = decrypt_value_str(os.environ['GRAPHACADEMY_DB_USER'])
+password = decrypt_value_str(os.environ['GRAPHACADEMY_DB_PW'])
+
+db_driver = GraphDatabase.driver(f"bolt://{host_port}", auth=(user, password), max_retry_time=15)
+
+community_content_query = """\
+MERGE (user:DiscourseUser {id: $params.topic.user_id })
+SET user.name = $params.topic.created_by.username,
+    user.avatarTemplate = $params.topic.created_by.avatar_template
+
+MERGE (topic:DiscourseTopic {id: $params.topic.id })
+SET topic.title = $params.topic.title,
+    topic.createdAt = datetime($params.topic.created_at),
+    topic.slug = $params.topic.slug,
+    topic.approved = $params.approved,
+    topic.rating = $params.rating
+
+MERGE (user)-[:POSTED_CONTENT]->(topic)
+"""
+
+
+def community_content(request, context):
+    headers = request["headers"]
+
+    event_type = headers["X-Discourse-Event-Type"]
+    event = headers["X-Discourse-Event"]
+
+    print(f"Received {event_type}: {event}")
+
+    body = request["body"]
+    json_payload = json.loads(body)
+    print(json_payload)
+
+    tags = json_payload["topic"]["tags"]
+    kudos_tags = [tag for tag in tags if tag.startswith("kudos")]
+
+    if len(kudos_tags) > 0:
+        json_payload["approved"] = True
+        json_payload["rating"] = int(kudos_tags[0].split("-")[-1])
+
+    with db_driver.session() as session:
+        result = session.run(community_content_query, {"params": json_payload})
+        print(result.summary().counters)
+
+    return {"statusCode": 200, "body": "Got the event", "headers": {}}
+
+
+user_events_query = """\
+MERGE (discourse:DiscourseUser {id: $params.user.id })
+SET discourse.name = $params.user.username,
+    discourse.location = $params.user.location,
+    discourse.avatarTemplate = $params.user.avatar_template,
+    discourse.screenName = $params.user.name
+WITH discourse
+OPTIONAL MATCH (user:User {auth0_key: $params.user.external_id})
+MERGE (user)-[:DISCOURSE_ACCOUNT]->(discourse)
+
+WITH user
+
+OPTIONAL MATCH (twitter:Twitter {screen_name: $params.user.user_fields.`4`})
+FOREACH (_ IN CASE twitter WHEN null THEN [] ELSE [1] END |
+   MERGE (user)-[:TWITTER_ACCOUNT]->(twitter))
+
+WITH user
+
+OPTIONAL MATCH (github:GitHub {name: $params.user.user_fields.`6`})
+FOREACH (_ IN CASE github WHEN null THEN [] ELSE [1] END |
+   MERGE (user)-[:GITHUB_ACCOUNT]->(github))
+
+
+WITH user
+so
+OPTIONAL MATCH (:StackOverflow {id: toInteger($params.user.user_fields.`5`)})
+FOREACH (_ IN CASE so WHEN null THEN [] ELSE [1] END |
+   MERGE (user)-[:STACKOVERFLOW_ACCOUNT]->(so))
+"""
 
 
 def user_events(request, context):
@@ -18,41 +96,10 @@ def user_events(request, context):
 
     body = request["body"]
     json_payload = json.loads(body)
-
     print(json_payload)
 
-    discourse_api_key = decrypt_value_str(os.environ['DISCOURSE_API_KEY'])
-    discourse_api_user = decrypt_value_str(os.environ['DISCOURSE_API_USER'])
-
-    user_id = json_payload["user"]["id"]
-
-    uri = f"https://community.neo4j.com/admin/users/{user_id}/groups"
-
-    payload = {
-        "api_key": discourse_api_key,
-        "api_user_name": discourse_api_user,
-        "group_id": "43"
-    }
-
-    print(payload)
-
-    m = MultipartEncoder(fields=payload)
-
-    r = requests.post(uri, data=m, headers={'Content-Type': m.content_type})
-    print(r)
+    with db_driver.session() as session:
+        result = session.run(user_events_query, {"params": json_payload})
+        print(result.summary().counters)
 
     return {"statusCode": 200, "body": "Got the event", "headers": {}}
-
-
-def decrypt_value(encrypted):
-    decrypted_response = boto3.client('kms').decrypt(CiphertextBlob=b64decode(encrypted))
-    return decrypted_response['Plaintext']
-
-
-def decrypt_value_str(encrypted):
-    decrypted_response = boto3.client('kms').decrypt(CiphertextBlob=b64decode(encrypted))
-    return decrypted_response['Plaintext'].decode("utf-8")
-
-
-def encrypt_value(value, kms_key):
-    return b64encode(boto3.client('kms').encrypt(Plaintext=value, KeyId=kms_key)["CiphertextBlob"])
